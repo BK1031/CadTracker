abaa51b1b45f7fe769ed92d1e7923e357946e192
Bharat Kathi <bharat1031@gmail.com>
2022-12-05 02:56:18 -0800

    initial skeleton

---
+++ lib/utils/config.dart
@@ -0,0 +1,13 @@
+import 'package:cad_tracker/models/version.dart';
+import 'package:fluro/fluro.dart';
+
+final router = FluroRouter();
+
+Version appVersion = Version("2.2.6+1");
+
+// ignore: non_constant_identifier_names
+// String API_HOST = "https://api.storkecentr.al";
+// String API_HOST = "http://localhost:4001";
+String API_HOST = "https://ce01-169-231-9-80.ngrok.io";
+// ignore: non_constant_identifier_names
+String SC_API_KEY = "sc-api-key";
